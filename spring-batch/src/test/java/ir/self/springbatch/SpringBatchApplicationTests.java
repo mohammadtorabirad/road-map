7d9df0e2b96901599dacd1b962bc14dd21f17ae6
M_Torabirad <mfISC275Admin##>
2024-02-04 10:32:09 +0330

    initial commit

---
+++ spring-batch/src/test/java/ir/self/springbatch/SpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.self.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
