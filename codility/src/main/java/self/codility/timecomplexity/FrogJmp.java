30aab0dd257fdd74f92f21cf20379f6a5264f79a
M_Torabirad <mfISC275Admin##>
2024-03-02 09:40:20 +0330

    Frog Jump

---
+++ codility/src/main/java/self/codility/timecomplexity/FrogJmp.java
@@ -0,0 +1,13 @@
+package self.codility.timecomplexity;
+
+public class FrogJmp {
+
+    public int solution(int X, int Y, int D){
+       int tmp = Y - X;
+       if (tmp % D ==0 ){
+           return tmp / D;
+       }else {
+           return (tmp / D) + 1;
+       }
+    }
+}
