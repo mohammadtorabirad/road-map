b89f4c80cc5fa0aaf4366bbc17b6436a16af0ba1
M_Torabirad <mfISC275Admin##>
2024-03-02 10:29:12 +0330

    find triangle

---
+++ codility/src/main/java/self/codility/sort/Triangle.java
@@ -0,0 +1,19 @@
+package self.codility.sort;
+
+import java.util.Arrays;
+
+public class Triangle {
+
+    public int solution(int[] A) {
+        int retVal = 0;
+        Arrays.sort(A);
+        for (int index = A.length - 1; index >= 2; index--) {
+            long tmp = A[index - 2] + A[index - 1];
+            if (tmp > A[index]) {
+                retVal = 1;
+                break;
+            }
+        }
+        return retVal;
+    }
+}
